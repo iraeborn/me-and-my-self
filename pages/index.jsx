520708107949b79807f7b93b644bc88fcca2681e
IraeBorn <iraeborn@gmail.com>
2021-09-23 14:45:35 -0400

    update infos

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,52 +21,66 @@ export default function Home() {
               <Card.Body>
                 <Card.Title>Sobre</Card.Title>
                 <Card.Text>
-                  Find in-depth information about Next.js features and API.
+                <ul>
+                  <li>36 anos</li>
+                  <li>Natural de Ilhabela, São Paulo</li>
+                  <li>Atualmente em Sorriso, MT</li>
+                </ul>
                 </Card.Text>
                 <Button variant="primary" href="https://nextjs.org/docs">
-                  More &rarr;
+                  Leia mais &rarr;
                 </Button>
               </Card.Body>
             </Card>
             <Card className="sml-card">
               <Card.Body>
-                <Card.Title>Learn</Card.Title>
+                <Card.Title>Skills</Card.Title>
                 <Card.Text>
-                  Learn about Next.js in an interactive course with quizzes!
+                <ul>
+                  <li>Gestão de Marketing digital</li>
+                  <li>Design Gráfico</li>
+                  <li>Adobe Photoshop</li>
+                  <li>Adobe Illustrator</li>
+                  <li>Direção de Fotografia</li>
+                  <li>Programação web</li>
+                </ul>
                 </Card.Text>
                 <Button variant="primary" href="https://nextjs.org/learn">
-                  More &rarr;
+                  Veja todas &rarr;
                 </Button>
               </Card.Body>
             </Card>
-          </Row>
-          <Row className="justify-content-md-between">
+
             <Card className="sml-card">
               <Card.Body>
-                <Card.Title>Examples</Card.Title>
+                <Card.Title>Portifólio</Card.Title>
                 <Card.Text>
-                  Discover and deploy boilerplate example Next.js projects.
+                  Alguns trabalhos e experimentos
                 </Card.Text>
                 <Button
                   variant="primary"
-                  href="https://github.com/vercel/next.js/tree/master/examples"
+                  href="https://github.com/iraeborn"
                 >
-                  More &rarr;
+                  Acesse meu github público &rarr;
                 </Button>
               </Card.Body>
             </Card>
+
             <Card className="sml-card">
               <Card.Body>
-                <Card.Title>Deploy</Card.Title>
+                <Card.Title>Contato</Card.Title>
                 <Card.Text>
-                  Instantly deploy your Next.js site to a public URL with
-                  Vercel.
+                <ul>
+                  <li>12 99222-6543 whatsapp</li>
+                  <li>iraeborn@gmail.com</li>
+                  <li>linked.in/in/iraebornholdt</li>
+                </ul>
                 </Card.Text>
                 <Button
                   variant="primary"
                   href="https://vercel.com/new?utm_source=github&utm_medium=example&utm_campaign=next-example"
                 >
-                  More &rarr;
+                  Bóra conversar &rarr;
                 </Button>
               </Card.Body>
             </Card>
@@ -76,12 +90,12 @@ export default function Home() {
 
       <footer className="cntr-footer">
         <a
-          href="https://vercel.com?filter=next.js&utm_source=github&utm_medium=example&utm_campaign=next-example"
+          href="https://www.instagram.com/iraeborn"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="sml-logo" />
+          @iraeborn
         </a>
       </footer>
     </Container>
